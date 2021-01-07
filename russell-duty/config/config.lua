11ed8798da083c404a5de970511055d31d0d2afa
RussellJW <76913433+RussellJW@users.noreply.github.com>
2021-01-07 23:54:25 +0100

    Update config.lua
    Removed my webhook link and imgur link.

--- russell-duty/config/config.lua
+++ russell-duty/config/config.lua
@@ -9,16 +9,16 @@ Config.Duty = {
 }
 
 Config.JobWH = {
-    ["ambulance"] = "https://discord.com/api/webhooks/767826203801157633/UCm3mx-35kibQU0b4k-VB1RSTqUwi4-Whx296TQTZEaiKRflZf_KpebiNzIU7Bmegykz",
-    ["offambulance"] = "https://discord.com/api/webhooks/767826203801157633/UCm3mx-35kibQU0b4k-VB1RSTqUwi4-Whx296TQTZEaiKRflZf_KpebiNzIU7Bmegykz",
-    ["police"] = "https://discord.com/api/webhooks/791018271855738918/6COeFT-wRA18_HOCgNULzQwb51XWuAbR6j5ric7-MlSKw4N2vlzBR8jwBBcUDtHnNRmf",
-    ["offpolice"] = "https://discord.com/api/webhooks/791018481474732082/6unOPUonRfdq0nmcQxbgBMzCOfrJ7U4BLyo_s_0U4kWUMt7KNSYiYXASVZXmTpST1v2M"
+    ["ambulance"] = "INSERT_WEBHOOK_HERE",
+    ["offambulance"] = "INSERT_WEBHOOK_HERE",
+    ["police"] = "INSERT_WEBHOOK_HERE",
+    ["offpolice"] = "INSERT_WEBHOOK_HERE"
 }
 
 Config.Bot = {
 
-	["Name"] = "Service helper",
-	["Image"] = "https://i.imgur.com/Ghk84lo.png"
+	["Name"] = "CHANGE_ME",
+	["Image"] = "INSERT_IMGUR_LINK"
 }
 
 Config.Jobs = {
